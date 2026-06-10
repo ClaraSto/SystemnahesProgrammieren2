7ae00cabb2225bc3e96da49b13832a924966824c
ClaraSto <clara.m.st@outlook.de>
2026-06-10 15:56:45 +0200

    Blatt3: Aufgabe 3

--- Blatt 3/code/src/rle.c
+++ Blatt 3/code/src/rle.c
@@ -1,31 +1,31 @@
 #include "../include/rle.h"
-#include <stdlib.h>
 #include <stdio.h>
+#include <stdlib.h>
 
 typedef struct RLENode {
-    uint64_t count;
-    struct RLENode* next;
+  uint64_t count;
+  struct RLENode *next;
 } RLENode;
 
 struct RLE {
-    RLENode* head;
-    RLENode* tail;
-    uint64_t size;
+  RLENode *head;
+  RLENode *tail;
+  uint64_t size;
 };
 
-static void append_to_rle(RLE* rle, uint64_t count) {
-    RLENode* node = malloc(sizeof(RLENode));
-    node->count = count;
-    node->next = NULL;
+static void append_to_rle(RLE *rle, uint64_t count) {
+  RLENode *node = malloc(sizeof(RLENode));
+  node->count = count;
+  node->next = NULL;
 
-    if (rle->tail) {
-        rle->tail->next = node;
-    } else {
-        rle->head = node;
-    }
+  if (rle->tail) {
+    rle->tail->next = node;
+  } else {
+    rle->head = node;
+  }
 
-    rle->tail = node;
-    rle->size += 1;
+  rle->tail = node;
+  rle->size += 1;
 }
 
 /**
@@ -35,15 +35,15 @@ static void append_to_rle(RLE* rle, uint64_t count) {
  * initialized with an entry.
  * @return a pointer to the RLE data structure
  */
-RLE* create_rle() {
-    RLE* rle = malloc(sizeof(RLE));
-    rle->head = NULL;
-    rle->tail = NULL;
-    rle->size = 0;
+RLE *create_rle() {
+  RLE *rle = malloc(sizeof(RLE));
+  rle->head = NULL;
+  rle->tail = NULL;
+  rle->size = 0;
 
-    append_to_rle(rle, 0); // Start with a count of 0 bits
+  append_to_rle(rle, 0); // Start with a count of 0 bits
 
-    return rle;
+  return rle;
 }
 
 /**
@@ -51,43 +51,43 @@ RLE* create_rle() {
  * should be called when the RLE data structure is no longer needed.
  * @param rle the RLE data structure to delete
  */
-void delete_rle(RLE* rle) {
-    RLENode* node = rle->head;
-    while (node) {
-        RLENode* next = node->next;
-        free(node);
-        node = next;
-    }
-    free(rle);
-}
-
-static bool pop_head_rle(RLE* rle, uint64_t* count) {
-    if (!rle->head) {
-        return false;
-    }
-
-    RLENode* node = rle->head;
-    *count = node->count;
-
-    rle->head = node->next;
-    if (rle->tail == node) {
-        rle->tail = NULL;
-    }
-
+void delete_rle(RLE *rle) {
+  RLENode *node = rle->head;
+  while (node) {
+    RLENode *next = node->next;
     free(node);
-    rle->size -= 1;
-
-    return true;
+    node = next;
+  }
+  free(rle);
 }
 
-static uint64_t get_rle_total_count(RLE* rle) {
-    uint64_t total = 0;
-    RLENode* node = rle->head;
-    while (node) {
-        total += node->count;
-        node = node->next;
-    }
-    return total;
+static bool pop_head_rle(RLE *rle, uint64_t *count) {
+  if (!rle->head) {
+    return false;
+  }
+
+  RLENode *node = rle->head;
+  *count = node->count;
+
+  rle->head = node->next;
+  if (rle->tail == node) {
+    rle->tail = NULL;
+  }
+
+  free(node);
+  rle->size -= 1;
+
+  return true;
+}
+
+static uint64_t get_rle_total_count(RLE *rle) {
+  uint64_t total = 0;
+  RLENode *node = rle->head;
+  while (node) {
+    total += node->count;
+    node = node->next;
+  }
+  return total;
 }
 
 /**
@@ -102,26 +102,27 @@ static uint64_t get_rle_total_count(RLE* rle) {
  * For example, if the start of data is "00001111", then the rle should contain
  * two entries 4 and 4.
  *
- * If the start of data is "11110000", then the rle should contain three entries,
- * 0, 4, and 4
+ * If the start of data is "11110000", then the rle should contain three
+ * entries, 0, 4, and 4
  * @param rle Will be filled with counts
  * @param data Source data, treated as binary data
  * @param size Size of the source data
  */
-void encode_rle(RLE* rle, const char* data, size_t size) {
-    uint8_t counting_bit = (rle->size & 1) ^ 1;
+void encode_rle(RLE *rle, const char *data, size_t size) {
+  uint8_t counting_bit = (rle->size & 1) ^ 1;
 
-    for (size_t i = 0; i < size; i++) {
-        for (int8_t j = 7; j >= 0; j--) {
-            uint8_t current_bit = (data[i] >> j) & 1;
-            if (current_bit == counting_bit) {
-                rle->tail->count++;;
-            } else {
-                append_to_rle(rle, 1);
-                counting_bit ^= 1; // Switch between 0 and 1
-            }
-        }
+  for (size_t i = 0; i < size; i++) {
+    for (int8_t j = 7; j >= 0; j--) {
+      uint8_t current_bit = (data[i] >> j) & 1;
+      if (current_bit == counting_bit) {
+        rle->tail->count++;
+        ;
+      } else {
+        append_to_rle(rle, 1);
+        counting_bit ^= 1; // Switch between 0 and 1
+      }
     }
+  }
 }
 
 /**
@@ -132,70 +133,75 @@ void encode_rle(RLE* rle, const char* data, size_t size) {
  * @param size will be set by this function and is the size of the returned data
  * @return binary data
  */
-char* decode_rle(RLE* rle, size_t* size) {
-    uint64_t total_bits = get_rle_total_count(rle);
-    *size = (total_bits + 7) >> 3; // Round up to the nearest byte
+char *decode_rle(RLE *rle, size_t *size) {
+  uint64_t total_bits = get_rle_total_count(rle);
+  *size = (total_bits + 7) >> 3; // Round up to the nearest byte
 
-    char* output = calloc(*size, sizeof(char));
-    if (!output) {
-        return NULL;
+  char *output = calloc(*size, sizeof(char));
+  if (!output) {
+    return NULL;
+  }
+
+  uint64_t count;
+  size_t byte_index = 0;
+
+  uint8_t bit = 0;
+  uint8_t bit_index = 7;
+
+  while (byte_index < *size && pop_head_rle(rle, &count)) {
+    while (count > 0) {
+      output[byte_index] |= bit << bit_index;
+      if (bit_index == 0) {
+        byte_index++;
+        bit_index = 7;
+      } else {
+        bit_index--;
+      }
+      count--;
     }
+    bit ^= 1; // Switch between 0 and 1
+  }
 
-    uint64_t count;
-    size_t byte_index = 0;
-
-    uint8_t bit = 0;
-    uint8_t bit_index = 7;
-
-    while (byte_index < *size && pop_head_rle(rle, &count)) {
-        while (count > 0) {
-            output[byte_index] |= bit << bit_index;
-            if (bit_index == 0) {
-                byte_index++;
-                bit_index = 7;
-            } else {
-                bit_index--;
-            }
-            count--;
-        }
-        bit ^= 1; // Switch between 0 and 1
-    }
-
-    return output;
+  return output;
 }
 
-void print_rle(RLE* rle, uint8_t counts_per_line) {
-    RLENode* node = rle->head;
-    printf("{\n");
-    int counter = 0;
-    while (node) {
-        printf("  %lu", node->count);
-        if (node->next) printf(", "); // print comma only if this isn't the last node
-        if (counts_per_line > 0 && ++counter >= counts_per_line) {
-            printf("\n");
-            counter = 0;
-        }
-
-        node = node->next;
+void print_rle(RLE *rle, uint8_t counts_per_line) {
+  RLENode *node = rle->head;
+  printf("{\n");
+  int counter = 0;
+  while (node) {
+    printf("  %lu", node->count);
+    if (node->next)
+      printf(", "); // print comma only if this isn't the last node
+    if (counts_per_line > 0 && ++counter >= counts_per_line) {
+      printf("\n");
+      counter = 0;
     }
-    printf(" }");
-    printf("\n");
+
+    node = node->next;
+  }
+  printf(" }");
+  printf("\n");
 }
 
-char* serialize_rle(RLE *rle, size_t* size) {
-    // TODO: Teilaufgabe 3
-    /**
-     * The `decode_rle` line is only added, so the function does something.
-     * You must remove it once you start implementing this function.
-     */
-     return decode_rle(rle, size);
+char *serialize_rle(RLE *rle, size_t *size) {
+  // TODO: Teilaufgabe 3
+  /**
+   * The `decode_rle` line is only added, so the function does something.
+   * You must remove it once you start implementing this function.
+   */
+  return decode_rle(rle, size);
 }
 
 void deserialize_rle(RLE *rle, const char *data, size_t size) {
-    // TODO: Teilaufgabe 3
-    /**
-     * The `encode_rle` line is only added, so the function does something.
-     * You must remove it once you start implementing this function.
-     */
-    encode_rle(rle, data, size);
+  // TODO: Teilaufgabe 3
+  size_t count_of_nodes = size / sizeof(uint64_t);
+
+  for (size_t i = 0; i < count_of_nodes; i++) {
+    uint64_t count = 0;
+    for (int b = 0; b < 8; b++) {
+      count = (count << 8) | (uint8_t)data[i * sizeof(uint64_t) + b];
+    }
+    append_to_rle(rle, count);
+  }
 }
